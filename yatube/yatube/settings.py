c103f6fdce273d4b63fb53bb24e61ac9d3d5ffa7
Nikolai <smirnovnikolai@yandex.ru>
2021-10-27 01:36:36 +0300

    Delet key

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd5&*i994#%m9!aov5n+3jd4t@k$ro5)it9@ufn^ssqudk5*6wc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
